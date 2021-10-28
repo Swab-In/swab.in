c67f7d48027749a8d6d06f3b6b7b3a7b68e0d4b7
Salmadea Fahira <salmadea.fahira01@ui.ac.id>
2021-10-28 17:15:15 +0700

    create list forum

--- forum/views.py
+++ forum/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Forum
+from .forms import ForumForm
 
 # Create your views here.
+def index(request):
+    return render(request, 'forum/index.html')
+
+def list(request):
+    forum = Forum.objects.all().values()
+    response = {'forums': forum}
+    return render(request, 'forum/list-forum.html')
