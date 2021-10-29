ee8c1e5604714ca718938e31df0e95df86bc7e8f
Umar Izzuddin <umar.izzuddin@ui.ac.id>
2021-10-29 12:22:45 +0700

    add komentar models

--- forum/models.py
+++ forum/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Komentar(models.Model):
+    komentar = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    
