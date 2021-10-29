47daa899d7515c7093b30933f318dec0e09c96f3
Umar Izzuddin <umar.izzuddin@ui.ac.id>
2021-10-29 21:30:58 +0700

    fix error on post data

--- swab_vaksin/forms.py
+++ swab_vaksin/forms.py
@@ -5,14 +5,4 @@ from swab_vaksin.models import SwabInformation, VaksinInformation, Experience
 class PengalamanForm(forms.ModelForm):
     class Meta:
         model = Experience
-        # fields = (
-        #     'pengalaman',
-        # )
-            
-
-        fields = "__all__"
-        # widget = {
-        #     'pengalaman' : Textarea
-        # }
-
-        # pengalaman = forms.CharField(label='', required=True, max_length=300, widget=forms.Textarea)
+        fields = ['pengalaman']
--- swab_vaksin/views.py
+++ swab_vaksin/views.py
@@ -3,6 +3,8 @@ from django.http.response import HttpResponseRedirect
 from swab_vaksin.models import SwabInformation, VaksinInformation, Experience
 from swab_vaksin.forms import PengalamanForm
 from django.shortcuts import render
+from .models import Experience
+from django.contrib.auth.models import User
 
 def index(request):
     return render(request, 'list_vaksin_swab.html')
@@ -11,12 +13,14 @@ def informasi(request):
     experiences = Experience.objects.all()
     response = {
         'experiences': experiences, 
-        'form': '',
         }
 
     form =  PengalamanForm(request.POST or None)
-    if (form.is_valid and request.method == 'POST'):
-        form.save()
+
+    if (form.is_valid() and request.method == 'POST'):
+        pengalaman = Experience.objects.create(penulis = User.objects.get(pk=request.user.id), 
+        pengalaman= form.cleaned_data["pengalaman"])
+        pengalaman.save()
         return HttpResponseRedirect('/swab-vaksin/informasi')
     
     response['form']= form
@@ -24,11 +28,11 @@ def informasi(request):
     return render(request, 'informasi_vaksin_swab.html', response)
 
 # def informasi(request):
-#     context = {}
-    form =  PengalamanForm(request.POST or None)
-    if (form.is_valid and request.method == 'POST'):
-        form.save()
-        return HttpResponseRedirect('/swab-vaksin/informasi')
+# #     context = {}
+#     form =  PengalamanForm(request.POST or None)
+#     if (form.is_valid and request.method == 'POST'):
+#         form.save()
+#         return HttpResponseRedirect('/swab-vaksin/informasi')
     
-    context['form']= form
+#     context['form']= form
 #     return render(request, 'informasi_vaksin_swab.html', context)
