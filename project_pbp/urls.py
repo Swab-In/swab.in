43610af350e2443ef049032dfdc458ce0201f9b5
Faiq <m.faiq@ui.ac.id>
2021-10-29 11:16:22 +0700

    Add app and routing

--- project_pbp/urls.py
+++ project_pbp/urls.py
@@ -13,11 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path, include
+from django.contrib import admin
 import swabIn.urls as swabIn
+import lokasi.urls as lokasi
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(swabIn))
+    path('', include(swabIn)),
+    path('lokasi/', include(lokasi))
 ]
