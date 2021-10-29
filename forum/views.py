21d1080a0c0e958b90653e490c0b4ca83869f9d1
Salmadea Fahira <salmadea.fahira01@ui.ac.id>
2021-10-29 12:20:46 +0700

    add forum form

--- forum/views.py
+++ forum/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
 from .models import Forum
 from .forms import ForumForm
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 def index(request):
     return render(request, 'forum/index.html')
 
+# masih error
 def list(request):
-    forum = Forum.objects.all().values()
-    response = {'forums': forum}
-    return render(request, 'forum/list-forum.html')
+    forum = Forum.objects.all()
+    form = ForumForm(request.POST or None, request.FILES or None)
+    if form.is_valid():
+        form.save()
+        if request.method == "POST" :
+            return HttpResponseRedirect('/')
+    context = {'form': form, 'forum': forum}
+    return render(request, "forum/list_forum.html", context)
