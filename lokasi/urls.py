49cf277accd3843093e0984034fa3f3032468d46
Faiq <m.faiq@ui.ac.id>
2021-10-29 14:43:46 +0700

    Refactoring

--- lokasi/urls.py
+++ lokasi/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
 from . import views
+from .views import PostListView, PostDetailView, PostCreateView
 
 urlpatterns = [
-    path('', views.index, name='lokasi')
+    path('', PostListView.as_view(), name='lokasi'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
 ]
--- lokasi/views.py
+++ lokasi/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
+)
 from .models import Post
 
+class PostListView(ListView):
+    model = Post
+    template_name = 'lokasi/index.html'  # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
 
-def index(request):
-    context = {
-        'posts': Post.objects.all()
-    }
-    return render(request, 'lokasi/index.html', context)
+class PostDetailView(DetailView):
+    model = Post 
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    # Set current author
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
