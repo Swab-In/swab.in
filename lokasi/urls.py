580a83fa4b8e06650c0829682ae23cac31efba04
Faiq <m.faiq@ui.ac.id>
2021-10-29 13:40:12 +0700

    Refactoring

--- lokasi/urls.py
+++ lokasi/urls.py
@@ -2,5 +2,5 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='lokasi')
+    path('', views.index, name='lokasi')
 ]
--- lokasi/views.py
+++ lokasi/views.py
@@ -1,24 +1,9 @@
 from django.shortcuts import render
-
-# Create your views here.
-posts = [
-    {
-        'author': 'John Mason',
-        'title': 'Tempat 1',
-        'content': 'First post content',
-        'date_posted': 'August 27, 2222'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Tempat 2',
-        'content': 'Second post content',
-        'date_posted': 'August 28, 2222'
-    }
-]
+from .models import Post
 
 
-def home(request):
+def index(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'lokasi/index.html', context)
