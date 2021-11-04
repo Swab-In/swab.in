4bddef5c795e412ff0d3ab6f4e258951607a7963
Faiq <m.faiq@ui.ac.id>
2021-11-04 13:39:54 +0700

    More refactoring

--- lokasi/models.py
+++ lokasi/models.py
@@ -11,7 +11,7 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.title
+        return self.lokasi
 
     def get_absolute_url(self):
         return reverse('lokasi')
