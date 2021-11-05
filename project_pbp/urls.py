784dcd76c39f4f7017eda66acaf3c2609bfa900d
Alviona Retno Amalia <72862324+alvionaretno@users.noreply.github.com>
2021-11-05 17:17:03 +0700

    Integrate User
    * perbaikan fitur log reg
    
    * log reg terbaruu
    
    * add userpic forms

--- project_pbp/urls.py
+++ project_pbp/urls.py
@@ -13,17 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path, include
 from django.contrib import admin
+from django.urls import path, include
 import swabIn.urls as swabIn
-import lokasi.urls as lokasi
 import user.urls as user
 import about.urls as about
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(swabIn)),
-    path('lokasi/', include(lokasi)),
     path('', include(user)),
     path('about/', include(about)),
 ]
