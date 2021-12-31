ff304125ed6a6e96efc597b8811da1daa3351f0b
nyoman.bagus02 <nyoman.bagus02@ui.ac.id>
2021-12-31 20:10:13 +0800

    ..

--- about/urls.py
+++ about/urls.py
@@ -4,6 +4,7 @@ from .views import *
 urlpatterns = [
     path('', index, name='index'),
     path('contact', contactus, name='contactus'),
-    path('json', json, name='json'),
-    path('list-message', listcontact, name='listcontact')
+    path('json', func_json, name='json'),
+    path('list-message', listcontact, name='listcontact'),
+    path('pesan-post', pesan_post, name='pesan_post')
 ]
--- about/views.py
+++ about/views.py
@@ -3,12 +3,13 @@ from django.shortcuts import render
 from .models import Pesan
 from .forms import ContactForm
 from django.core import serializers
+from django.views.decorators.csrf import csrf_exempt
+import json
 
 # Create your views here.
 def index(request):
     return render(request, 'about.html')
 
-# @login_required(login_url='/admin/login/')
 def contactus(request):
     context = {}
 
@@ -22,9 +23,30 @@ def contactus(request):
     context['form'] = form
     return render(request, 'contact.html', context)
 
-def json(request):
+def func_json(request):
     data = serializers.serialize('json', Pesan.objects.all())
     return HttpResponse(data, content_type="application/json")
 
 def listcontact(request):
     return render(request, 'list_message.html')
+
+@csrf_exempt
+def pesan_post(request):
+    body_unicode = request.body.decode('utf-8')
+
+    body = json.loads(body_unicode)
+
+    first = body['first']
+    last = body['last']
+    email = body['email']
+    no_hp = body['no_hp']
+    message = body['message']
+
+    pesan = Pesan.objects.create(first, last, email, no_hp, message)
+    pesan.save()
+
+    return HttpResponse(pesan, content_type="application/json", status=201)
+
+
+
+
