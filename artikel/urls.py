6e219031cdda9e41c80865149481469da1465be8
paskahponchius <paskahponchius@gmail.com>
2022-01-01 20:43:25 +0700

    mau ngepull

--- artikel/urls.py
+++ artikel/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import HomeView, ArticleDetail, artikel_content, json_req
+from .views import HomeView, ArticleDetail, json_req, add_comment, artikel_cards, get_comment
 
 urlpatterns = [
     path('/', HomeView.as_view() , name='index'),
     path('<int:pk>', ArticleDetail.as_view(), name='detailArtikel'),
     path('/json_req', json_req , name='json_req'),
-    path('/artikel_content', artikel_content , name='artikel_content'),
+    path('/artikel_cards', artikel_cards , name='artikel_cards'),   
+    path('/add_comment', add_comment , name='add_comment'),
+    path('/get_comment', get_comment , name='get_comment'),
+
 ]
